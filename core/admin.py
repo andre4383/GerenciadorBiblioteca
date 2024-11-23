1c85e0d119040b137318548fbba58efd0b8de162
André Montenegro <126609983+andre4383@users.noreply.github.com>
2024-11-23 16:41:12 -0300

    Delete core directory

--- core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Autor, Categoria, Livro, Emprestimo
-
-admin.site.register(Autor)
-admin.site.register(Categoria)
-admin.site.register(Livro)
-admin.site.register(Emprestimo)
